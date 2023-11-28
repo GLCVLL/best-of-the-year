789ce45fbdeaf94eaacd88a14c40cb4d0a118da5
Gioele Iacovella <g.ioele96@outlook.it>
2023-11-28 15:36:32 +0100

    added GetMapping method on Movie and Song classes

--- src/main/java/org/java/spring/controller/MainController.java
+++ src/main/java/org/java/spring/controller/MainController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.java.model.Movie;
+import org.java.model.Song;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,6 +25,44 @@ public class MainController {
         movies.add(new Movie(2, "Schindler's List"));
         movies.add(new Movie(3, "Forrest Gump"));
         return movies;
+    }
+    
+    private List<Song> getBestSongs() {
+        List<Song> songs = new ArrayList<>();
+        songs.add(new Song(1, "Bohemian Rhapsody"));
+        songs.add(new Song(2, "Imagine"));
+        songs.add(new Song(3, "Like a Rolling Stone"));
+        return songs;
+    }
+    
+    // MOVIE METHOD
+    
+    @GetMapping("/movies")
+    
+    public String getMovies(Model model) {
+    	List<Movie> m = getBestMovies();
+    	List<String> mt = new ArrayList<>();
+        for (Movie movie : m) {
+            mt.add(movie.getTitle());
+        }
+        
+        model.addAttribute("titles", mt);
+        
+        return "movies";
+    	
+    }
+    
+    // SONG METHOD
+    
+    @GetMapping("/songs")
+    public String getSongs(Model model) {
+        List<Song> s = getBestSongs();
+        List<String> st = new ArrayList<>();
+        for (Song song : s) {
+            st.add(song.getTitle());
+        }
+        model.addAttribute("titles", st);
+        return "songs";
     }
 
 }
