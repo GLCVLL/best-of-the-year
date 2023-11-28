fc90b97716a22bdd0c611b646cd4eb12e93217df
Gioele Iacovella <g.ioele96@outlook.it>
2023-11-28 14:44:54 +0100

    added Movie and Song classes and method to get them

---
+++ src/main/java/org/java/model/Song.java
@@ -0,0 +1,26 @@
+package org.java.model;
+
+public class Song {
+	
+	private int id;
+	private String title;
+	
+	public Song(int id, String title) {
+		
+		this.id = id;
+		this.title = title;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+
+	
+	
+
+}
--- src/main/java/org/java/spring/controller/MainController.java
+++ src/main/java/org/java/spring/controller/MainController.java
@@ -1,5 +1,9 @@
 package org.java.spring.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.java.model.Movie;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +16,14 @@ public class MainController {
     public String home(Model model) {
         model.addAttribute("author", "Gio");
         return "home";
+    }
+    
+    private List<Movie> getBestMovies() {
+        List<Movie> movies = new ArrayList<>();
+        movies.add(new Movie(1, "Il Padrino"));
+        movies.add(new Movie(2, "Schindler's List"));
+        movies.add(new Movie(3, "Forrest Gump"));
+        return movies;
     }
 
 }
